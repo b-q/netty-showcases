192f2ed49c82daebb73f9e3638bec188755d6dee
b-q <>
2022-04-01 20:39:55 +0200

    Time Protocol Implement(stream-based) : use Decoder to deal with fragmentation issue

--- netty-client/src/main/java/handler/TimeClientHandler.java
+++ netty-client/src/main/java/handler/TimeClientHandler.java
@@ -7,29 +7,16 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import java.util.Date;
 
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
-    private ByteBuf buf;
-
-    @Override
-    public void handlerAdded(ChannelHandlerContext ctx) {
-        buf = ctx.alloc().buffer(4);
-    }
-
-    @Override
-    public void handlerRemoved(ChannelHandlerContext ctx) {
-        buf.release();
-        buf = null;
-    }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ByteBuf m = (ByteBuf) msg;
-        buf.writeBytes(m);
-        m.release();
-
-        if (buf.readableBytes() >= 4) {
-            long currentTimeMillis = (buf.readUnsignedInt() - 2208988800L) * 1000L;
+        ByteBuf m = (ByteBuf) msg; // (1)
+        try {
+            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
             System.out.println(new Date(currentTimeMillis));
             ctx.close();
+        } finally {
+            m.release();
         }
     }
 
