a079db950f06d60d622da78567b2dabf9c3bbee0
b-q <>
2022-04-01 20:41:20 +0200

    Time Protocol Implement(stream-based) : use ReplayingDecoder to deal with fragmentation issue

--- netty-client/src/main/java/TimeDecoder.java
+++ netty-client/src/main/java/TimeDecoder.java
@@ -1,16 +1,13 @@
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.ReplayingDecoder;
 
 import java.util.List;
 
-public class TimeDecoder extends ByteToMessageDecoder {
+public class TimeDecoder extends ReplayingDecoder<Void> {
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
-        if (in.readableBytes() < 4) {
-            return;
-        }
-
+    protected void decode(
+            ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
         out.add(in.readBytes(4));
     }
 }
